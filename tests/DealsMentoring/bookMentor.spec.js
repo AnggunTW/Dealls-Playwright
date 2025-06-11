83e9c8131beb3894bce0eeaff8a80f4957c5859e
AnggunTW <sactestqa@gmail.com>
2025-06-11 23:05:04 +0700

    Update bookMentor.spec.js

--- tests/DealsMentoring/bookMentor.spec.js
+++ tests/DealsMentoring/bookMentor.spec.js
@@ -16,10 +16,25 @@ test('User can book a mentoring session', async ({ page }) => {
   await page.locator('.MentorCard_mentor_card__zMRXB').first().click();
 
   // Book a session
-  //const availableSlot = page.getByText(/PM|AM/).first(); // assuming time slot has "PM" or "AM"
-  //await availableSlot.click();
-  //await page.getByRole('button', { name: 'Book Session' }).click();
+    await page.click('button:has-text("Ajukan Jadwal")');
+    await page.click('button:has-text("LPDP")');
+    await page.click('button:has-text("Selanjutnya")');
 
   // Confirmation
-  //await expect(page.getByText('Booking confirmed')).toBeVisible();
+    await page.click('button:has-text("Select Date Range")');
+    await page.waitForSelector('text=June 2025');
+    await page.locator('[aria-label="Choose Friday June 20 of 2025"]').click();
+    await page.locator('[aria-label="Choose Sunday June 22 of 2025"]').click();
+ //navigate to month of june 2025
+    while (!(await page.locator('text=June 2025').isVisible())) {
+    await page.locator('button[aria-label="Next month"]').click();}
+// Input Hour and minute
+    await page.getByPlaceholder('HH:MM').first().fill('10:00');
+    await page.getByPlaceholder('HH:MM').nth(1).fill('12:00');
+    await page.click('button:has-text("Selanjutnya")');
+
+//next page
+    await page.getByLabel('Saya telah memposting ulang postingan di atas & tag 3 teman di bagian komentar').check();
+    await page.getByLabel("I commit to attend the schedule I’ve chosen & understand the failure to attend 2x may result to suspension").check();
+    await page.click('button:has-text("Selesai")');
 });
